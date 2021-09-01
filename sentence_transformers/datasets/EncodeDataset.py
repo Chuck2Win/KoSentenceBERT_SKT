dd2f62df5443ddd2ec9bc2ee1673abfd08c96c6a
Chuck2Win <53412437+Chuck2Win@users.noreply.github.com>
2021-09-01 20:14:26 +0900

    Add files via upload

--- sentence_transformers/datasets/EncodeDataset.py
+++ sentence_transformers/datasets/EncodeDataset.py
@@ -7,7 +7,7 @@ class EncodeDataset(Dataset):
     def __init__(self,
                  sentences: Union[List[str], List[int]],
                  model: SentenceTransformer,
-                 is_tokenized: bool = True):
+                 is_tokenized: bool = True, length_check: int = 128):
         """
         EncodeDataset is used by SentenceTransformer.encode method. It just stores
         the input texts and returns a tokenized version of it.
@@ -15,10 +15,11 @@ class EncodeDataset(Dataset):
         self.model = model
         self.sentences = sentences
         self.is_tokenized = is_tokenized
+        self.length_check = length_check
 
 
     def __getitem__(self, item):
-        return self.sentences[item] if self.is_tokenized else self.model.tokenize(self.sentences[item])
+        return self.sentences[item][:self.length_check-2] if self.is_tokenized else self.model.tokenize(self.sentences[item])[:self.length_check-2]
 
 
     def __len__(self):
