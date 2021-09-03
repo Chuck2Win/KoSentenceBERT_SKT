e08566107a8f18029bb417f2198c21516390115c
Chuck2Win <53412437+Chuck2Win@users.noreply.github.com>
2021-09-03 09:54:54 +0900

    Update Transformer.py

--- sentence_transformers/models/Transformer.py
+++ sentence_transformers/models/Transformer.py
@@ -63,8 +63,7 @@ class Transformer(nn.Module):
         """
         Tokenizes a text and maps tokens to token-ids
         """
-        tokens = self.tokenizer(text)
-        tokens = [self.vocab.token_to_idx[token] for token in tokens]
+        tokens = self.tokenizer.convert_tokens_to_ids(text)
         return tokens
 
     def get_segment_ids_vaild_len(self, inputs):
